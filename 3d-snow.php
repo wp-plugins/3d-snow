201c26963fd6d0407f8521c5f57b7508bcf600ea
creavince <creavince@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-15 14:45:04 +0000

    Commit 0.1 stable
    git-svn-id: https://plugins.svn.wordpress.org/3d-snow/tags/0.1@822487 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ 3d-snow.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: 3D Snow
+Plugin Script: 3d-snow.php
+Plugin URI: http://www.vanmeerdervoort.nl/3d-snow-wordpress-plugin
+Description: An awesome 3D snow effect for your website! Just install and activate to make it snow.
+Version: 0.1
+License: GPL
+Author: <a href="http://www.vanmeerdervoort.nl">vanMeerdervoort</a>
+Author URI: http://www.vanmeerdervoort.nl/
+
+=== RELEASE NOTES ===
+2013-12-10 - v0.1 - first version
+*/
+
+/* 
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+Online: http://www.gnu.org/licenses/gpl.txt
+*/
+
+
+function threeDsnow_add_scripts() {
+	
+	wp_register_script('three', plugins_url('/js/Three.js', __FILE__), array('jquery'),'1.1', true);
+	wp_register_script('snow', plugins_url('/js/Snow.js', __FILE__), array('jquery'),'1.1', true);
+	wp_register_script('snowinit', plugins_url('/js/SnowInit.js', __FILE__), array('jquery'),'1.1', true);
+
+	wp_enqueue_script('three');
+	wp_enqueue_script('snow');
+	wp_enqueue_script('snowinit');
+}
+
+add_action( 'wp_enqueue_scripts', 'threeDsnow_add_scripts' );  
+
+function threeDsnow_add_enddiv(){
+
+echo "</div>";
+}
+
+if ( !is_admin() ) {
+
+?>
+
+<div id="showBox" style="position: absolute; top: -10px; left: 0px; z-index: 0; width: 100%; height: 1px;background-color:#fff">
+<canvas width="1" height="1" style="z-index:-1;"></canvas>
+<?
+
+add_action('wp_footer', 'threeDsnow_add_enddiv');
+
+}
+
+?>
